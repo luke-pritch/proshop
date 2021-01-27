e9d922b3afe0b28701aabddd432e4c465e04a8eb
Luke Pritchard <luke-pritch@users.noreply.github.com>
2021-01-27 14:52:35 -0500

    init repo

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+
+    sourceType: 'module', // Allows for the use of imports
+    exmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'], // Fixes resolving the path for .ts or .tsx files
+      },
+    },
+  },
+
+  extends: [
+    'airbnb',
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] }, // Keeping ESLint happy about jsx in tsx
+    ],
+    'import/extensions': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-empty-interface': 0,
+    '@typescript-eslint/no-use-before-define': ['error'],
+  },
+};
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+interface Props {}
+
+const Footer = (props: Props) => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            Copyright &copy; Luke Pritchard 2021
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
